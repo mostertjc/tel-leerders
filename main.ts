49580bc1d0488ef3fb12a9e79de4828f461fcecc
mostertjc <102650119+mostertjc@users.noreply.github.com>
2023-07-01 07:29:10 +0200

    2 nuwe sleutels is bygevoeg.

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-let Getal = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     Getal += 1
+})
+input.onButtonPressed(Button.AB, function () {
     basic.showNumber(Getal)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    Getal += -1
 })
+let Getal = 0
+Getal = 0
